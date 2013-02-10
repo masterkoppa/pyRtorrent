81624c9413430e74abd44f59dfd48eff413378da
Andres J Ruiz Torres <andresruiz2010@gmail.com>
2013-02-09 22:20:54 -0500

    Make a test rTorrent information page. Kind of a log in...

--- MainWindow.py
+++ MainWindow.py
@@ -1,48 +1,70 @@
 import sys
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
+
 
 class MainWindow(QtGui.QWidget):
+	
+	url = None
+	username = None
+	password = None
+
+	urlEdit = None
+	usernameEdit = None
+	passwordEdit = None
 
 	def __init__(self):
 		super(MainWindow, self).__init__()
+		self.initUI()
+		
+	def initUI(self):
+		
+		self.url = QtGui.QLabel('RPC URL')
+		self.username = QtGui.QLabel('Username')
+		self.password = QtGui.QLabel('Password')
 
-		self.build_and_show_ui()
+		self.urlEdit = QtGui.QLineEdit()
+		self.usernameEdit = QtGui.QLineEdit()
+		self.passwordEdit = QtGui.QLineEdit()
 
-	def build_and_show_ui(self):
-		names = ['Cls', 'Bck', '', 'Close', '7', '8', '9', '/',
-				'4', '5', '6', '*', '1', '2', '3', '-',
-				'0', '.', '=', '+']
+		self.passwordEdit.setEchoMode(QtGui.QLineEdit.Password)
+
+		submit = QtGui.QPushButton('OK')
 
 		grid = QtGui.QGridLayout()
+		grid.setSpacing(5)
 
-		j = 0
-		pos = [(0, 0), (0, 1), (0, 2), (0, 3),
-				(1, 0), (1, 1), (1, 2), (1, 3),
-				(2, 0), (2, 1), (2, 2), (2, 3),
-				(3, 0), (3, 1), (3, 2), (3, 3 ),
-				(4, 0), (4, 1), (4, 2), (4, 3)]
+		grid.addWidget(self.url, 1, 0)
+		grid.addWidget(self.urlEdit, 1, 1)
 
-		for i in names:
-			button = QtGui.QPushButton(i)
-			button.clicked.connect(self.calcButton)
-			if j == 2:
-				grid.addWidget(QtGui.QLabel(''), 0, 2)
-			else: grid.addWidget(button, pos[j][0], pos[j][1])
-			j = j + 1
+		grid.addWidget(self.username, 2, 0)
+		grid.addWidget(self.usernameEdit, 2, 1)
 
-		self.setLayout(grid)   
+		grid.addWidget(self.password, 3, 0)
+		grid.addWidget(self.passwordEdit, 3, 1)
+
+		#Add the submit button
+		grid.addWidget(submit, 4,1)
 		
-		self.move(300, 150)
-		self.setWindowTitle('Calculator')    
+		self.setLayout(grid)
+
+		submit.clicked.connect(self.submitLogin)
+		
+		self.setGeometry(300, 300, 350, 300)
+		self.setWindowTitle('pyRtorrent')
 		self.show()
 
-	def calcButton(self):
-		sender = self.sender()
-
+	def submitLogin(self):
+		print("Pressed OK!")
+		print(self.urlEdit.text())
+		print(self.usernameEdit.text())
+		print(self.passwordEdit.text())
+		
+def main():
 	
-
-if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
 	main = MainWindow()
-
 	sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+	main()
